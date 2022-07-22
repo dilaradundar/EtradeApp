aa6981e455078584fa7f714556739f79d03026ec
ddundar <ddundar@N107900>
2022-07-22 11:39:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleAppEnginD
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+
+}
